1c98b22a4114b5c822613a249574a26fc539f2f4
WeiboPan <1525695648@qq.com>
2024-11-15 20:21:00 +0800

    1

---
+++ designPatternCode/designPatternCode/commandMode/src/main/java/com/atguigu/commandmode/CommandModeApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.commandmode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandModeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommandModeApplication.class, args);
+    }
+
+}
---
+++ designPatternCode/designPatternCode/commandMode/src/test/java/com/atguigu/commandmode/CommandModeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.commandmode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandModeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

ab17696a473326c7f5af21dca9a5d4259086ec68
WeiboPan <1525695648@qq.com>
2024-11-20 23:30:16 +0800

    1

---
+++ designPatternCode/designPatternCode/delegationMode/src/main/java/com/atguigu/delegationmode/DelegationModeApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.delegationmode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelegationModeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelegationModeApplication.class, args);
+    }
+
+}
